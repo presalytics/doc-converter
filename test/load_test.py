7c1df5525c3518f89b907288fb4be5ac2ff6e23b
kevin-presealytics <kevin@presalytics.io>
2022-02-04 10:01:42 -0800

    add task queue

---
+++ test/load_test.py
@@ -0,0 +1,17 @@
+import requests
+import uuid
+from pathlib import Path
+
+
+test_file = Path(__file__).parent.joinpath("files").joinpath("Rectangle.pptx")
+
+for i in range(0, 20):
+    r = requests.post('http://localhost:8080/convert', files={
+        'file': ('Rectangle.pptx', open(test_file, 'rb'), 'application/vnd.openxmlformats-officedocument.presentationml.presentation'),
+        'userId': (None, str(uuid.uuid4())),
+        'id': (None, str(uuid.uuid4())),
+        'convertType': (None, 'svg'),
+        'storyId': (None, str(uuid.uuid4())),
+    })
+
+    print(r.status_code)
